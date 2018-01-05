006664953166263384ac5c7fbef1496d1e142e09
pennyworthit <alfredeung@gmail.com>
2018-01-05 06:01:48 +0000

    modified helpers.php

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -6,7 +6,7 @@ function get_db_config() {
 
         return $db_config = [
             'connection' => 'pgsql',
-            'host' => $url("host"),
+            'host' => $url["host"],
             'database' => substr($url["path"], 1),
             'username' => $url["user"],
             'password' => $url["pass"],
